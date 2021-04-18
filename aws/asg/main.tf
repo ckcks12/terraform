585d84593e9ba937088f9a2cf2b819b972d4b55b
3unch4n <3unch4n@gmail.com>
2021-04-18 18:02:34 +0900

    add: .idea

---
+++ aws/asg/main.tf
@@ -0,0 +1,111 @@
+data "aws_ami" "linux2" {
+  owners      = ["amazon"]
+  most_recent = true
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami*gp2"]
+  }
+}
+
+resource "aws_launch_template" "this" {
+  name          = var.name
+  image_id      = data.aws_ami.linux2.id
+  instance_type = var.instance_type
+  ebs_optimized = var.ebs_optimized
+
+  block_device_mappings {
+    device_name = var.volume_device_name
+
+    ebs {
+      volume_type           = var.volume_type
+      volume_size           = var.volume_size
+      delete_on_termination = true
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  iam_instance_profile {
+    name = var.iam_name
+  }
+
+  monitoring {
+    enabled = true
+  }
+
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = var.sg_ids
+    delete_on_termination       = true
+  }
+
+  key_name = var.key_name
+
+  user_data = var.user_data
+}
+
+resource "aws_autoscaling_group" "asg" {
+  name = var.name
+
+  min_size         = var.node_min_cnt
+  desired_capacity = var.node_desired_cnt
+  max_size         = var.node_max_cnt
+
+  vpc_zone_identifier = var.subnet_ids
+
+  launch_template {
+    id      = aws_launch_template.this.id
+    version = "$Latest"
+  }
+
+  health_check_type         = ""
+  health_check_grace_period = 10
+  default_cooldown          = 300
+  termination_policies = [
+    "OldestLaunchConfiguration",
+    "OldestInstance"
+  ]
+
+  enabled_metrics = [
+    "GroupMinSize",
+    "GroupMaxSize",
+    "GroupDesiredCapacity",
+    "GroupInServiceInstances",
+    "GroupPendingInstances",
+    "GroupStandbyInstances",
+    "GroupTerminatingInstances",
+    "GroupTotalInstances",
+  ]
+
+  tags = concat([for k, v in var.tag : {
+    key                 = k,
+    value               = v,
+    propagate_at_launch = true
+    }], [
+    {
+      key                 = "Name",
+      value               = var.name,
+      propagate_at_launch = true
+    }
+  ])
+
+  depends_on = [aws_launch_template.this]
+}
